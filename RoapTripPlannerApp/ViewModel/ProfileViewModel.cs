eed7d68dd7ad07740ec128b416f643c29711bd7b
Leo Ortin <153555453+LeoOrtin15@users.noreply.github.com>
2024-05-09 09:43:32 +0100

    Add update details in ProfilePage

--- RoapTripPlannerApp/ViewModel/ProfileViewModel.cs
+++ RoapTripPlannerApp/ViewModel/ProfileViewModel.cs
@@ -4,6 +4,7 @@ using RoapTripPlannerApp.Model;
 using RoapTripPlannerApp.Services;
 using RoapTripPlannerApp.View;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace RoapTripPlannerApp.ViewModel;
 
@@ -19,23 +20,35 @@ public partial class ProfileViewModel : ObservableObject
     public ProfileViewModel(DatabaseService database)
     {
         this.database = database;
+        LoadUserDetails();
+    }
+    async void LoadUserDetails()
+    {
+        UserModel user = await database.GetLoggedInUser();
+        Username = user.Username;
+        Email = user.Email;
+        Password = user.Password;
     }
 
     [RelayCommand]
+    async Task UpdateDetails()
+    {
+        if (string.IsNullOrWhiteSpace(Username) || string.IsNullOrEmpty(Email) || string.IsNullOrWhiteSpace(Password))
+        {
+            await App.Current.MainPage.DisplayAlert("Mandatory Fields Empty", "'Email', 'Username' and 'Password' are mandatory fields", "OK");
+        }
+        else
+        {
+            await database.UpdateUserDetails(Email, Username, Password);
+            await App.Current.MainPage.DisplayAlert("User Details Updated", "User details have been updated successfully", "OK");
+        }
+    }
+    [RelayCommand]
     async Task LogOut()
     {
-        // Search in the database for the user that is logged in
-        List<UserModel> users = await database.GetAllUsers();
-        foreach (UserModel user in users)
-        {
-            if (user.IsUserLoggedIn)
-            {
-                // Log out the user
-                user.IsUserLoggedIn = false;
-                await database.UpdateUserDetails(user);
-                break;
-            }
-        }
+        // Log out the user
+        await database.LogOutUser();
+        // Redirect to LoginPage
         await Shell.Current.GoToAsync($"/{nameof(LoginPage)}");
     }
 }
