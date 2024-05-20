1228d848f1ccf240af382d8e93b246c670acd6a3
Leo Ortin <153555453+LeoOrtin15@users.noreply.github.com>
2024-05-20 20:41:34 +0100

    Add LoginTest

---
+++ RoadTripPlannerAppTest/LoginTest.cs
@@ -0,0 +1,53 @@
+﻿using Moq;
+using RoadTripPlannerApp.Model;
+using RoadTripPlannerApp.Services; 
+using RoadTripPlannerApp.ViewModel;
+
+namespace RoadTripPlannerAppTest
+{
+    public class LoginTest
+    {
+        [Fact]
+        public async Task Login_ValidInput_SuccessfulLogin()
+        {
+            //// Arrange
+            //var databaseService = new DatabaseMockService();
+            //var alert = new Mock<IAlertService>();
+            //var loginViewModel = new LoginViewModel(databaseService, alert.Object);
+            //loginViewModel.Username = "user1"; // Registered user
+            //loginViewModel.Password = "password1"; // Correct password
+            //// Act
+            //await loginViewModel.LoginCommand.ExecuteAsync(null);
+            //// Assert
+            //Assert.NotNull(await databaseService.GetLoggedInUser());
+        }
+        [Fact]
+        public async Task Login_NotRegisteredUser_UnableToLogin()
+        {
+            // Arrange
+            var databaseService = new DatabaseMockService();
+            var alert = new Mock<IAlertService>();
+            var loginViewModel = new LoginViewModel(databaseService, alert.Object);
+            loginViewModel.Username = "test"; //Unregistered user
+            loginViewModel.Password = "test";
+            // Act
+            await loginViewModel.LoginCommand.ExecuteAsync(null);
+            // Assert
+            Assert.Equivalent(await databaseService.GetLoggedInUser(), new UserModel());
+        }
+        [Fact]
+        public async Task Login_IncorrectPassword_UnableToLogin()
+        {
+            // Arrange
+            var databaseService = new DatabaseMockService();
+            var alert = new Mock<IAlertService>();
+            var loginViewModel = new LoginViewModel(databaseService, alert.Object);
+            loginViewModel.Username = "user1"; //Registered user
+            loginViewModel.Password = "test"; // Incorrect password
+            // Act
+            await loginViewModel.LoginCommand.ExecuteAsync(null);
+            // Assert
+            Assert.Equivalent(await databaseService.GetLoggedInUser(), new UserModel());
+        }
+    }
+}
